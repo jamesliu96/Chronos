09e509d70c457d0dbb5e2b2ecbc67e3669c8e89e
James Liu <jamesliu96@users.noreply.github.com>
2024-08-21 19:26:42 +0800

    chronos

--- app/src/main/java/net/carapax/chronos/MainActivity.kt
+++ app/src/main/java/net/carapax/chronos/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Context.LOCATION_SERVICE
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
@@ -24,15 +25,19 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
@@ -43,6 +48,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -54,6 +60,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
@@ -99,6 +106,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     val context = LocalContext.current
+    val configuration = LocalConfiguration.current
 
     val locationManager = context.getSystemService(LOCATION_SERVICE) as LocationManager
 
@@ -178,7 +186,32 @@ fun App() {
 
     ChronosTheme {
         Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
-            Column {
+            if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) Row {
+                AnimatedVisibility(
+                    verbose,
+                    enter = slideInHorizontally { it } + fadeIn(),
+                    exit = slideOutHorizontally { it } + fadeOut(),
+                ) {
+                    SmallFloatingActionButton({ magic = !magic }) {
+                        AnimatedContent(
+                            if (magic) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                            transitionSpec = { scaleIn() + fadeIn() togetherWith scaleOut() + fadeOut() },
+                            label = ""
+                        ) {
+                            Icon(it, null)
+                        }
+                    }
+                }
+                SmallFloatingActionButton({ verbose = !verbose }) {
+                    AnimatedContent(
+                        if (verbose) Icons.Default.Close else Icons.Default.Settings,
+                        transitionSpec = { scaleIn() + fadeIn() togetherWith scaleOut() + fadeOut() },
+                        label = ""
+                    ) {
+                        Icon(it, null)
+                    }
+                }
+            } else Column {
                 AnimatedVisibility(
                     verbose,
                     enter = slideInVertically { it } + fadeIn(),
@@ -205,51 +238,99 @@ fun App() {
                 }
             }
         }) { innerPadding ->
-            Column(
+            Surface(
                 modifier = Modifier
                     .padding(innerPadding)
                     .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                LocationTime(
-                    now = now,
-                    label = stringResource(R.string.system),
-                    local = true,
-                    verbose = verbose,
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                LocationTime(
-                    now = now,
-                    label = stringResource(R.string.gps),
-                    locationTime = gpsLocationTime,
-                    unique = true,
-                    verbose = verbose,
-                    tick = magic,
-                )
-                AnimatedVisibility(verbose) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Spacer(modifier = Modifier.height(8.dp))
-                        LocationTime(
-                            now = now,
-                            label = stringResource(R.string.network),
-                            locationTime = netLocationTime,
-                            verbose = verbose,
-                        )
-                    }
-                }
-                AnimatedVisibility(!locationPermissionsState.allPermissionsGranted) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Button({
-                            if (locationPermissionsState.shouldShowRationale) locationPermissionsState.launchMultiplePermissionRequest()
-                            else context.startAppSettings()
-                        }) {
-                            Text(
-                                if (locationPermissionsState.permissions.size == locationPermissionsState.revokedPermissions.size) stringResource(
-                                    R.string.grant_permissions
-                                ) else stringResource(R.string.allow_fine_location)
+                if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    LocationTime(
+                        now = now,
+                        label = stringResource(R.string.system),
+                        local = true,
+                        verbose = verbose,
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    LocationTime(
+                        now = now,
+                        label = stringResource(R.string.gps),
+                        locationTime = gpsLocationTime,
+                        unique = true,
+                        verbose = verbose,
+                        tick = magic,
+                    )
+                    AnimatedVisibility(verbose) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Spacer(modifier = Modifier.width(8.dp))
+                            LocationTime(
+                                now = now,
+                                label = stringResource(R.string.network),
+                                locationTime = netLocationTime,
+                                verbose = verbose,
                             )
+                        }
+                    }
+                    AnimatedVisibility(!locationPermissionsState.allPermissionsGranted) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Button({
+                                if (locationPermissionsState.shouldShowRationale) locationPermissionsState.launchMultiplePermissionRequest()
+                                else context.startAppSettings()
+                            }) {
+                                Text(
+                                    if (locationPermissionsState.permissions.size == locationPermissionsState.revokedPermissions.size) stringResource(
+                                        R.string.grant_permissions
+                                    ) else stringResource(R.string.allow_fine_location)
+                                )
+                            }
+                        }
+                    }
+                } else Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    LocationTime(
+                        now = now,
+                        label = stringResource(R.string.system),
+                        local = true,
+                        verbose = verbose,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    LocationTime(
+                        now = now,
+                        label = stringResource(R.string.gps),
+                        locationTime = gpsLocationTime,
+                        unique = true,
+                        verbose = verbose,
+                        tick = magic,
+                    )
+                    AnimatedVisibility(verbose) {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Spacer(modifier = Modifier.height(8.dp))
+                            LocationTime(
+                                now = now,
+                                label = stringResource(R.string.network),
+                                locationTime = netLocationTime,
+                                verbose = verbose,
+                            )
+                        }
+                    }
+                    AnimatedVisibility(!locationPermissionsState.allPermissionsGranted) {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Button({
+                                if (locationPermissionsState.shouldShowRationale) locationPermissionsState.launchMultiplePermissionRequest()
+                                else context.startAppSettings()
+                            }) {
+                                Text(
+                                    if (locationPermissionsState.permissions.size == locationPermissionsState.revokedPermissions.size) stringResource(
+                                        R.string.grant_permissions
+                                    ) else stringResource(R.string.allow_fine_location)
+                                )
+                            }
                         }
                     }
                 }
@@ -272,66 +353,73 @@ private fun LocationTime(
 
     val fixedLength by animateIntAsState(if (verbose) 3 else 1, label = "")
 
-    AnimatedVisibility(verbose) {
-        Text(label, fontSize = 16.sp)
-    }
-    if (local) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
         AnimatedVisibility(verbose) {
-            Text(
-                now.formatLocalDate(),
-                fontSize = 16.sp,
-            )
+            Text(label, fontSize = 16.sp)
         }
-        Text(
-            now.formatLocalTime(fixedLength).annotateMs(),
+        if (local) {
+            AnimatedVisibility(verbose) {
+                Text(
+                    now.formatLocalDate(),
+                    fontSize = 16.sp,
+                )
+            }
+            Text(
+                now.formatLocalTime(fixedLength).annotateMs(),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        } else if (locationTime != null) {
+            val elapsed = now - locationTime.then
+            val timestamp = locationTime.time + elapsed
+            val delta = now - timestamp
+            AnimatedVisibility(verbose) {
+                Text(
+                    timestamp.formatLocalDate(),
+                    fontSize = 16.sp,
+                )
+            }
+            Text(
+                timestamp.formatLocalTime(fixedLength).annotateMs(),
+                fontSize = (if (unique) 40 else 32).sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                delta.formatSecond(fixedLength),
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = (if (unique) 24 else 16).sp,
+            )
+            Text(
+                elapsed.formatSecond(fixedLength),
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 8.sp,
+                fontStyle = FontStyle.Italic,
+                lineHeight = 1.em,
+            )
+            AnimatedVisibility(verbose) {
+                Text(
+                    locationTime.location.string,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontSize = 6.sp,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 1.em,
+                )
+            }
+            if (tick) LaunchedEffect(timestamp.epochSeconds) {
+                view.performHapticFeedback(HapticFeedbackConstantsCompat.CLOCK_TICK)
+            }
+        } else Text(
+            if (!verbose) stringResource(
+                R.string.x_not_available, label
+            ) else stringResource(R.string.not_available),
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.error,
         )
-    } else if (locationTime != null) {
-        val elapsed = now - locationTime.then
-        val timestamp = locationTime.time + elapsed
-        val delta = now - timestamp
-        AnimatedVisibility(verbose) {
-            Text(
-                timestamp.formatLocalDate(),
-                fontSize = 16.sp,
-            )
-        }
-        Text(
-            timestamp.formatLocalTime(fixedLength).annotateMs(),
-            fontSize = (if (unique) 40 else 32).sp,
-            fontWeight = FontWeight.Bold,
-        )
-        Text(
-            delta.formatSecond(fixedLength),
-            color = MaterialTheme.colorScheme.primary,
-            fontSize = (if (unique) 24 else 16).sp,
-        )
-        Text(
-            elapsed.formatSecond(fixedLength),
-            color = MaterialTheme.colorScheme.secondary,
-            fontSize = 8.sp,
-            fontStyle = FontStyle.Italic,
-            lineHeight = 1.em,
-        )
-        Text(
-            locationTime.location.format(verbose),
-            color = MaterialTheme.colorScheme.tertiary,
-            fontSize = 6.sp,
-            textAlign = TextAlign.Center,
-            lineHeight = 1.em,
-        )
-        if (tick) LaunchedEffect(timestamp.epochSeconds) {
-            view.performHapticFeedback(HapticFeedbackConstantsCompat.CLOCK_TICK)
-        }
-    } else Text(
-        if (!verbose) stringResource(
-            R.string.x_not_available, label
-        ) else stringResource(R.string.not_available),
-        fontSize = 24.sp,
-        fontWeight = FontWeight.Bold,
-        color = MaterialTheme.colorScheme.error,
-    )
+    }
 }
 
 @Composable
@@ -388,6 +476,7 @@ private fun Instant.formatLocalDate() = LocalDateTime.Format {
     char('-')
     dayOfMonth()
 }.format(this.localDateTime)
+
 private fun Instant.formatLocalTime(fixedLength: Int = 3) = LocalDateTime.Format {
     hour()
     char(':')
@@ -412,45 +501,38 @@ private val Long.instant get() = Instant.fromEpochMilliseconds(this)
 private operator fun Long.plus(duration: Duration) = this.instant + duration
 private fun Long.formatSecond(fixedLength: Int = 3) = (this / 1E3).fixed(fixedLength)
 
-private fun Location.format(verbose: Boolean = false) = buildString {
-    append("${latitude}°/$longitude°")
-    if (verbose) {
+private val Location.string
+    get() = buildString {
+        append("${latitude}°/$longitude°")
         append(' ')
         append('(')
         append(if (hasAccuracy()) "${accuracy}m" else "N/A")
         append('/')
         append(if (hasVerticalAccuracy()) "${verticalAccuracyMeters}m" else "N/A")
         append(')')
-    }
-    append('\n')
-    append(if (hasAltitude()) "${altitude}m" else "N/A")
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
         append('\n')
-        append(if (hasMslAltitude()) "${mslAltitudeMeters}m" else "N/A")
-        if (verbose) {
+        append(if (hasAltitude()) "${altitude}m" else "N/A")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            append('\n')
+            append(if (hasMslAltitude()) "${mslAltitudeMeters}m" else "N/A")
             append(' ')
             append('(')
             append(if (hasMslAltitudeAccuracy()) "${mslAltitudeAccuracyMeters}m" else "N/A")
             append(')')
         }
-    }
-    append('\n')
-    append(if (hasBearing()) "${bearing}°" else "N/A")
-    if (verbose) {
+        append('\n')
+        append(if (hasBearing()) "${bearing}°" else "N/A")
         append(' ')
         append('(')
         append(if (hasBearingAccuracy()) "${bearingAccuracyDegrees}°" else "N/A")
         append(')')
-    }
-    append('\n')
-    append(if (hasSpeed()) "${speed}m/s" else "N/A")
-    if (verbose) {
+        append('\n')
+        append(if (hasSpeed()) "${speed}m/s" else "N/A")
         append(' ')
         append('(')
         append(if (hasSpeedAccuracy()) "${speedAccuracyMetersPerSecond}m/s" else "N/A")
         append(')')
     }
-}
 
 private val Context.settings by preferencesDataStore("settings")
 private fun Context.startAppSettings() = startActivity(
